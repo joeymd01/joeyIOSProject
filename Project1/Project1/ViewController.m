cb9c634fec37f287da4034d7fa137346eb4180fb
Joey Dennehy <joeydennehy@Joeys-MacBook-Air.local>
2013-01-25 15:25:59 -0700

    committing project 1

--- Project1/Project1/ViewController.m
+++ Project1/Project1/ViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ViewController
 
+int pressCount = 1;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +26,24 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)pressButton
+{
+    ++pressCount;
+    
+    if(pressCount % 2 == 0)
+    {
+        ProjLabel.text = @"I was Pressed";
+    }
+    if(pressCount % 3 == 0)
+    {
+        ProjLabel.text = @"Stop Pressing Me";
+    }
+    if(pressCount % 4 == 0)
+    {
+        ProjLabel.text = @"I'm calling UI protective services";
+    }
 }
 
 @end
