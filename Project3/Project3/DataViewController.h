8fe2144de720ca3f33c927345d88297d337c2ec2
Joey Dennehy <joeydennehy@Joeys-MacBook-Air.local>
2013-02-05 20:09:40 -0700

    Project 3 final

---
+++ Project3/Project3/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  test2
+//
+//  Created by Joey Dennehy on 2/4/13.
+//  Copyright (c) 2013 Joey Dennehy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
---
+++ Project3/Project3/ModelController.h
@@ -0,0 +1,18 @@
+//
+//  ModelController.h
+//  test2
+//
+//  Created by Joey Dennehy on 2/4/13.
+//  Copyright (c) 2013 Joey Dennehy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+
+@end
